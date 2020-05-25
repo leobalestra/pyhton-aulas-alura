82fed8e46dcbcea4ae2829e5966305db38a2e888
leobalestra <leonardodiasbalestra@gmail.com>
2020-05-25 19:43:48 -0300

    Comparação

--- AulasPython/Igualdade.py
+++ AulasPython/Igualdade.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
         self._codigo = codigo
@@ -10,6 +13,9 @@ class ContaSalario:
 
         return(self._codigo == other._codigo)
 
+    def __lt__(self, other):
+        return self._saldo < other._saldo
+
     def deposita(self, valor):
         self._saldo += valor
 
@@ -18,5 +24,11 @@ class ContaSalario:
 
 conta1 = ContaSalario(37)
 conta2 = ContaSalario(37)
+contas = [conta1, conta2]
 
+#fucionou por causa do __eq__
 print(conta1 == conta2)
+
+#funcionou por causa do __lt__
+for conta in sorted(contas):
+    print(conta)
